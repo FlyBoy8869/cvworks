4964ac411e1bc789c98e75964e94fb33baed9110
Charles Cognato <charlescognato@gmail.com>
2024-01-18 18:32:21 -0500

    Make styling separate.

--- cvworks_gui/view.py
+++ cvworks_gui/view.py
@@ -1,6 +1,4 @@
-import os
 import datetime
-import platform
 
 from PyQt6.QtCore import QDate, Qt
 from PyQt6.QtGui import QPixmap
@@ -8,50 +6,7 @@ from PyQt6.QtWidgets import QWidget, QMessageBox
 
 import schedule.schedule as schedule
 from cvworks_gui.ui.calendar_ui import Ui_Form
-
-from version import VERSION
-
-BASEDIR = os.path.dirname(__file__)
-
-WORKS = {True: "WORKING", False: "OFF"}
-
-WORKS_STYLESHEET = (
-    "background-color: red; color: yellow; "
-    "border: 2px solid yellow; border-radius: 6px; font-weight: bold;"
-)
-OFF_STYLESHEET = (
-    "background-color: green; color: white; "
-    "border: 2px solid white; border-radius: 6px; font-weight: bold;"
-)
-STYLESHEET = {True: WORKS_STYLESHEET, False: OFF_STYLESHEET}
-
-# About Dialog
-INFO_ICON = os.path.join(BASEDIR, "info.png")
-
-INFO_DIALOG_HEADING_WEIGHT = 500
-INFO_DIALOG_TEXT_SIZE = 10
-INFO_DIALOG_TEXT_FONT_WEIGHT = 300
-
-AUTHOR_HEADING = f"<p><span style='font-weight: {INFO_DIALOG_HEADING_WEIGHT};'>Written by:</span><br>"
-AUTHOR_NAME = (
-    f"<span style='font-weight: {INFO_DIALOG_TEXT_FONT_WEIGHT}; "
-    f"font-size: {INFO_DIALOG_TEXT_SIZE}px;'>Charles Cognato</span><br>"
-)
-AUTHOR_EMAIL = (
-    f"<span style='font-weight: {INFO_DIALOG_TEXT_FONT_WEIGHT}; "
-    f"font-size: {INFO_DIALOG_TEXT_SIZE}px;'>charlescognato@gmail.com</span></p>"
-)
-VERSION_HEADING = (
-    f"<p><span style='font-weight: {INFO_DIALOG_HEADING_WEIGHT};'>Version:</span><br>"
-)
-OPERATING_SYSTEM = (
-    f"<span style='font-weight: {INFO_DIALOG_TEXT_FONT_WEIGHT}; "
-    f"font-size: {INFO_DIALOG_TEXT_SIZE}px;'>{platform.platform().split('-', 1)[0]}</span><br>"
-)
-PROGRAM_VERSION = (
-    f"<span style='font-weight: {INFO_DIALOG_TEXT_FONT_WEIGHT}; "
-    f"font-size: {INFO_DIALOG_TEXT_SIZE}px;'>{VERSION}</span></p>"
-)
+from cvworks_gui.styling import *
 
 
 class CalendarView(QWidget, Ui_Form):
