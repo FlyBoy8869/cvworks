f8485e1f6993e42345b51329d6e5abc44371f443
Charles Cognato <charlescognato@gmail.com>
2025-01-13 14:30:21 -0500

    Commit adds a new build script to update the version info in the pyproject.toml file.

---
+++ buildscripts/update-pyproject-toml-version.py
@@ -0,0 +1,14 @@
+with open("_version.py", mode="rt") as input_file:
+    version_info = input_file.readline().split("=")[1].strip()
+    version_info.replace('"', "")
+
+pyproject_toml_contents = []
+with open("pyproject.toml", mode="rt") as input_file:
+    for line in input_file:
+        if line.startswith("version"):
+            pyproject_toml_contents.append(f'version = {version_info}\n')
+        else:
+            pyproject_toml_contents.append(line)
+
+with open("pyproject.toml", mode="wt") as output_file:
+    output_file.writelines(pyproject_toml_contents)
