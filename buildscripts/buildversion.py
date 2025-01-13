bec189e2493150a778782de48a748d26278b2096
Charles Cognato <charlescognato@gmail.com>
2025-01-13 14:29:39 -0500

    Commit update to use lowercase version.

--- buildscripts/buildversion.py
+++ buildscripts/buildversion.py
@@ -7,7 +7,7 @@ version = (
     f"{d.year}.{d.month:02d}.{d.day:02d} - build {t.hour:02d}{t.minute:02d}.{t.second}"
 )
 
-m_text = f"VERSION = '{version}'\n"
+m_text = fVERSION = f'version = "{version}"\n'
 
 with open("_version.py", "wt", encoding="utf-8") as outfile:
     outfile.write(m_text)
