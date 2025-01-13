51ffd42969d5487801602c5226b3b82539a1fb03
Charles Cognato <charlescognato@gmail.com>
2025-01-13 17:38:00 -0500

    Commit attempts to fix pyproject.toml versioning problem (but don't know until I try it).

--- buildscripts/update-pyproject-toml-version.py
+++ buildscripts/update-pyproject-toml-version.py
@@ -1,12 +1,16 @@
 with open("_version.py", mode="rt") as input_file:
     version_info = input_file.readline().split("=")[1].strip()
     version_info.replace('"', "")
+    build_info = input_file.readline().split("=")[1].strip()
+    build_info.replace('"', "")
 
 pyproject_toml_contents = []
 with open("pyproject.toml", mode="rt") as input_file:
     for line in input_file:
         if line.startswith("version"):
             pyproject_toml_contents.append(f'version = {version_info}\n')
+        elif line.startswith("build_version"):
+            pyproject_toml_contents.append(f'build_build = {build_info}\n')
         else:
             pyproject_toml_contents.append(line)
 
