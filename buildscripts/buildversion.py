c1aac5513fcbe2282f30d89c3e371aa54f96e3e2
Charles Cognato <charlescognato@gmail.com>
2025-01-13 17:28:49 -0500

    Commit attempts to fix pyproject.toml versioning problem (but don't know until I try it).

--- buildscripts/buildversion.py
+++ buildscripts/buildversion.py
@@ -4,10 +4,14 @@ now = datetime.now()
 d = now.date()
 t = now.time()
 version = (
-    f"{d.year}.{d.month:02d}.{d.day:02d} - build {t.hour:02d}{t.minute:02d}.{t.second}"
+    f"{d.year}.{d.month:02d}.{d.day:02d}"
 )
 
-m_text = fVERSION = f'version = "{version}"\n'
+build = f"{t.hour:02d}.{t.minute:02d}.{t.second}"
+
+m_version = f'version = "{version}"\n'
+m_build = f'build = "{build}"\n'
 
 with open("_version.py", "wt", encoding="utf-8") as outfile:
-    outfile.write(m_text)
+    outfile.write(m_version)
+    outfile.write(m_build)
