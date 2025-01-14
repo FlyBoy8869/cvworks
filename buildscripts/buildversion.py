7adb8e92a6319a11dac44ec3fde7c50c57e00546
Charles Cognato <charlescognato@gmail.com>
2025-01-14 06:44:55 -0500

    Trying different version formats.

--- buildscripts/buildversion.py
+++ buildscripts/buildversion.py
@@ -4,10 +4,10 @@ now = datetime.now()
 d = now.date()
 t = now.time()
 version = (
-    f"{d.year}.{d.month:02d}.{d.day:02d}"
+    f"{d.year}.{d.month:02d}"
 )
 
-build = f"{t.hour:02d}.{t.minute:02d}.{t.second}"
+build = f"{d.day:02d}{t.hour:02d}{t.minute:02d}{t.second}"
 
 m_version = f'version = "{version}"\n'
 m_build = f'build = "{build}"\n'
